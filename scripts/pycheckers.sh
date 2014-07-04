236fe02ae9bb8953bbc3d675802fac8dc9a7a5f2
Glen Nelson <glen.nelson@emc.com>
2014-07-04 14:29:30 -0600

    Update pycheckers script - badly - for venv support

--- scripts/pycheckers.sh
+++ scripts/pycheckers.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # From http://stackoverflow.com/questions/1259873/how-can-i-use-emacs-flymake-mode-for-python-with-pyflakes-and-pylint-checking-co
 
+if [ -n $2 ];
+then
+    . /local/home/nelsog4/venv/$2/bin/activate
+fi
 epylint "$1" 2>/dev/null
 pyflakes "$1"
-pep8 --ignore=E221,E701,E202,E501 --repeat "$1"
-pep257 "$1"
+pep8 --repeat "$1"
+pep257 "$1" > /dev/null
+if [ -n $2 ];
+then
+    deactivate
+fi
 true
